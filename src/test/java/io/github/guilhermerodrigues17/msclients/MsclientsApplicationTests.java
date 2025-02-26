7113baa407da7d1f3d92519af88150399cae445c
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-02-26 16:34:11 -0300

    Initial commit

---
+++ src/test/java/io/github/guilhermerodrigues17/msclients/MsclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guilhermerodrigues17.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
