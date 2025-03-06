274c1c293af56e191e4e18f4063a9109c976956e
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-06 18:33:39 -0300

    feat: Implementadas operações post (save) e get (getClientByCpf) para o endpoint 'clients'

--- src/main/java/io/github/guilhermerodrigues17/msclients/application/ClientsResource.java
+++ src/main/java/io/github/guilhermerodrigues17/msclients/application/ClientsResource.java
@@ -1,15 +1,45 @@
 package io.github.guilhermerodrigues17.msclients.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.guilhermerodrigues17.msclients.application.representation.ClientRequestRepresentation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clients")
+@RequiredArgsConstructor
 public class ClientsResource {
+
+    private final ClientsService service;
 
     @GetMapping
     public String status() {
         return "OK!";
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody ClientRequestRepresentation representation) {
+        var client = representation.toModel();
+        service.save(client);
+
+        URI headerLocation = ServletUriComponentsBuilder.fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(client.getCpf())
+                .toUri();
+
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<Object> getClientByCpf(@RequestParam String cpf) {
+        var client = service.getByCpf(cpf);
+        if (client.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(client);
     }
 }
