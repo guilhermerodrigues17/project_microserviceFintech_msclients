ebf89660788a0ddd515c6a7fc8d201fb340e43d4
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-06 18:04:29 -0300

    feat: Implementada entidade Client na camada de domain

---
+++ src/main/java/io/github/guilhermerodrigues17/msclients/domain/Client.java
@@ -0,0 +1,29 @@
+package io.github.guilhermerodrigues17.msclients.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String cpf;
+    private String name;
+    private Integer age;
+
+    public Client(String cpf, String name, Integer age) {
+        this.cpf = cpf;
+        this.name = name;
+        this.age = age;
+    }
+}
